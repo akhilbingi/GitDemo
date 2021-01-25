c69acae5e95b283d15c222a8db31906d28f1cc95
Sanghavi bingi <bingis@Sanghavis-MacBook-Air.local>
2021-01-25 10:35:15 -0500

    This is my first commit for maven java project

---
+++ src/test/java/MvnFun/mavenJava1/agentsTest.java
@@ -0,0 +1,18 @@
+package MvnFun.mavenJava1;
+
+import org.junit.Test;
+
+public class agentsTest {
+	@Test
+	public void IndependentAgents() {
+    
+	 
+		System.out.println("This is the list of all the policy numbers of IndependentAgents");
+		
+	}
+	@Test
+	public void DependentAgents() {
+		System.out.println("This is the list of all the policy  commissions of DependentAgents");
+	}
+
+}
